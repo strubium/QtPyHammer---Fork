f20acdcc6731e552974dbdb6dbc33c66a7142d8d
strubium <113206902+strubium@users.noreply.github.com>
2024-04-19 17:43:11 -0500

    Error popup!

--- QtPyHammer/ui/core.py
+++ QtPyHammer/ui/core.py
@@ -46,9 +46,6 @@ class MainWindow(QtWidgets.QMainWindow):
         self.actions["File/Save As"] = file_menu.addAction("Save &As")
         def save_file_as(): ops.save_file_as(self, self.map_browser)
         self.actions["File/Save As"].triggered.connect(save_file_as)
-        self.actions["File/Error Test"] = file_menu.addAction("Example Popup (for testing)")
-        error_popup = popup.browser(parent=self,text="woow")
-        self.actions["File/Error Test"].triggered.connect(error_popup.show)
         self.actions["File/Texture Test"] = file_menu.addAction("Texture Menu (for testing)")
         texture_popup = texture_browser.TextureBrowser(parent=self)
         self.actions["File/Texture Test"].triggered.connect(texture_popup.show)
@@ -123,7 +120,7 @@ class MainWindow(QtWidgets.QMainWindow):
             ent_browser = entity.browser(parent=self)
             self.actions["Tools/Brush to Entity"].triggered.connect(ent_browser.show)
         except Exception as exc:
-            error_popup = popup.browser(parent=self)
+            error_popup = popup.browser(parent=self,text="Failed to load .fgds!")
             self.actions["Tools/Brush to Entity"].triggered.connect(error_popup.show)
             self.actions["Tools/Brush to Entity"].setEnabled(False)
             raise exc
