b682467b5cfe35d2ccc0465b3e87b247498e9504
strubium <113206902+strubium@users.noreply.github.com>
2024-04-19 17:22:22 -0500

    Revert "Edit Toolbar"
    This reverts commit 7537bd882de15ed5da4b84776d0909c93c2dbfa0.

--- QtPyHammer/ui/core.py
+++ QtPyHammer/ui/core.py
@@ -329,10 +329,6 @@ class MainWindow(QtWidgets.QMainWindow):
         # cut copy paste | cordon | TL <TL> | DD 3D DW DA |
         # compile helpers 2D_models fade CM prop_detail NO_DRAW
 
-        edit_tools = QtGui.QToolBar("Edit Tools")
-        edit_tools.setMovable(True)
-        self.addToolBar(QtCore.Qt.LeftToolBarArea, toolbarBox)
-
     def open(self, filename):  # allows loading via drag & drop
         raw_filename, extension = os.path.splitext(filename)
         short_filename = os.path.basename(filename)
