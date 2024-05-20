e8a67cab1713291bef2414e89febc968595bde65
strubium <113206902+strubium@users.noreply.github.com>
2024-05-20 11:20:44 -0500

    Add render mode

--- QtPyHammer/ui/properties.py
+++ QtPyHammer/ui/properties.py
@@ -5,9 +5,20 @@ class browser(QtWidgets.QDialog):
     def __init__(self, parent):
         super(browser, self).__init__(parent, QtCore.Qt.Tool)
         self.setWindowTitle("Properties")
+        
+        self.box1 = QtWidgets.QLabel("Render Mode")
+        self.render_mod_combo_box = QtWidgets.QComboBox()
+        self.render_mod_combo_box.setGeometry(200, 150, 120, 40)
+        self.render_mod_combo_box.addItem("Flat")
+        self.render_mod_combo_box.addItem("Textured")
+        self.render_mod_combo_box.addItem("Wireframe")
+        self.render_mod_combo_box.setCurrentIndex(self.render_mod_combo_box.findText("Textured"))
 
         # Layout setup
         base_layout = QtWidgets.QVBoxLayout()
+        base_layout.addWidget(self.box1)
+        base_layout.addWidget(self.render_mod_combo_box)
+        
         bottom_row = QtWidgets.QHBoxLayout()
         bottom_row.addStretch(1)
         ok_button = QtWidgets.QPushButton(lang.langOk())
