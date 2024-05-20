b969aade77f713e87b9b30979e60bb4c1e8b0800
strubium <113206902+strubium@users.noreply.github.com>
2024-05-20 11:34:04 -0500

    Language menu

--- QtPyHammer/ui/properties.py
+++ QtPyHammer/ui/properties.py
@@ -14,15 +14,27 @@ class browser(QtWidgets.QDialog):
         self.render_mod_combo_box.addItem("Wireframe")
         self.render_mod_combo_box.setCurrentIndex(self.render_mod_combo_box.findText("Textured"))
 
+        self.box2 = QtWidgets.QLabel("Language:")
+        self.lang_combo_box = QtWidgets.QComboBox()
+        self.lang_combo_box.setGeometry(200, 150, 120, 40)
+        self.lang_combo_box.addItem("English")
+        self.lang_combo_box.addItem("Spanish")
+        self.lang_combo_box.addItem("German")
+        self.lang_combo_box.addItem("Italian")
+        self.lang_combo_box.addItem("Russian")
+        self.lang_combo_box.setCurrentIndex(self.lang_combo_box.findText("English"))
+
         # Layout setup
         base_layout = QtWidgets.QVBoxLayout()
         base_layout.addWidget(self.box1)
         base_layout.addWidget(self.render_mod_combo_box)
+        base_layout.addWidget(self.box2)
+        base_layout.addWidget(self.lang_combo_box)
         
         bottom_row = QtWidgets.QHBoxLayout()
         bottom_row.addStretch(1)
         ok_button = QtWidgets.QPushButton(lang.langOk())
-        ok_button.clicked.connect(self.accept)
+        ok_button.clicked.connect(self.on_ok_clicked)
         ok_button.setDefault(True)
         bottom_row.addWidget(ok_button)
         base_layout.addLayout(bottom_row)
@@ -30,3 +42,8 @@ class browser(QtWidgets.QDialog):
 
         # Resize the dialog to fit the text
         self.adjustSize()
+    
+    
+    def on_ok_clicked(self):
+        render_mod_index = self.render_mod_combo_box.currentIndex()
+        lang.setLanguage(self.lang_combo_box.text())
